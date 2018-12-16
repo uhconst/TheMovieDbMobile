674b331b9a78df48c9d42791cd17b4e1c7db0c27
Uryel <uryelhenrique.c@gmail.com>
2018-12-16 16:42:25 -0200

    Add test to verify if when a list item is clicked the MovieDetailsActivity is opened

---
+++ app/src/androidTest/java/com/uhc/themoviedbmobile/MainActivityTest.java
@@ -0,0 +1,37 @@
+package com.uhc.themoviedbmobile;
+
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.uhc.themoviedbmobile.activity.MainActivity;
+import com.uhc.themoviedbmobile.activity.MovieDetailsActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Created by const on 16/12/18.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+    @Rule
+    public IntentsTestRule<MainActivity> activity_rule = new IntentsTestRule<>(MainActivity.class);
+
+    /**
+     * Verify if when a list item is clicked the activity MovieDetailsActivity is opened
+     */
+    @Test
+    public void verifyListItemClick_MainActivity() {
+        onView(withId(R.id.rv_movies))
+                .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        intended(hasComponent(MovieDetailsActivity.class.getName()));
+    }
+}

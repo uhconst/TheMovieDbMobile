44af60714538d02ef80bccde1370a5169c84b317
Uryel <uryelhenrique.c@gmail.com>
2018-12-18 22:12:18 -0200

    Add functions to format date and get current date time

--- app/src/main/java/com/uhc/themoviedbmobile/utils/TMDMUtils.java
+++ app/src/main/java/com/uhc/themoviedbmobile/utils/TMDMUtils.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 /**
  * Created by const on 12/17/18.
  */
@@ -13,5 +18,20 @@ public class TMDMUtils {
         NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
 
         return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+    public static String formatDate(String data) {
+        if (data != null && data.contains("-"))
+            try {
+                return new SimpleDateFormat("MM/dd/yyyy", Locale.US).format(new SimpleDateFormat("yyyy-MM-dd", Locale.US).parse(data));
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        return data;
+    }
+
+    public static String getCurrentDateTime() {
+        return new SimpleDateFormat("MM/dd/yyyy HH:mm", Locale.US).format(Calendar.getInstance().getTime());
     }
 }
